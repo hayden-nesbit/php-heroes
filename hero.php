0110dfeaf47c6ffe80f139777bb2f98a32f22715
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-04-14 17:07:39 -0400

    working on hero page

--- hero.php
+++ hero.php
@@ -12,10 +12,16 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $hero = "hero.php?id=" . $row["id"];
         echo "<h3> $row[name]</h3></a>" . $row["biography"] . "<br>" . "<br>";
+        echo "<br";
+        echo "<a href='index.php'>". "Back" . "</a>";
     }
 } else {
     echo "0 results";
 }
+
+$sql = "SELECT * FROM relationships WHERE hero1_id = $id OR hero2_id = $id AND type_id = 1";
+$result = $conn->query($sql);
+var_dump($result) ;
 
 $conn->close();
 
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require "connection.php";
 require "header.php";
 
-echo "<h1>Hello Supers!</h1>";
+echo "<h1 class='text-center mt-5 mb-4'>Hello Supers!</h1>";
 
 $sql = "SELECT * FROM heroes";
 $result = $conn->query($sql);
@@ -11,21 +11,24 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $output = "";
     echo '<div class="container pl-5">';
+    echo '<div class="row">';
     while ($row = $result->fetch_assoc()) {
         $hero = "hero.php?id=" . $row["id"];
         $output .=
-            '<div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." />
+            '<div class="col-4">
+                <div class="card mb-3 bg-light" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">' . $row["name"] . '</h5>
                         <p class="card-text">' . $row["about_me"] . '</p>
                         <a href=' . $hero . ' class="btn btn-primary">About me</a>
+                    </div>
                     </div>
                 </div>';
     }
 
     echo $output;
     echo '</div>';
+   
 } else {
     echo "0 results";
 }
@@ -36,12 +39,3 @@ $conn->close();
 </body>
 
 </html>
-
-<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-</div>
